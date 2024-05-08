7dacef3354a64f7f96a674491896dd9e3516988e
samarendrabehera.papu@gmail.com <samarendrabehera.papu@gmail.com>
2024-05-08 08:07:03 +0530

    update

--- StackDSA/getMin.cpp
+++ StackDSA/getMin.cpp
@@ -1,50 +1,94 @@
-#include<iostream>
-#include<stack>
+#include <iostream>
+#include <stack>
 using namespace std;
 
-class specialStack{
+class specialStack
+{
     stack<int> st;
     int mini = INT_MAX;
 
-    void push(int element){
-        if(st.empty()){
+    void push(int element)
+    {
+        if (st.empty())
+        {
             mini = element;
             st.push(element);
-        }else{
-            if(mini < element){
+        }
+        else
+        {
+            if (mini < element)
+            {
                 st.push(element);
-            }else{
+            }
+            else
+            {
                 int val = 2 * element - mini;
                 st.push(val);
                 mini = element;
             }
-
         }
     }
-    int pop(){
-        if(st.top()==-1){
+    int pop()
+    {
+        if (st.top() == -1)
+        {
             return -1;
-        }else{
+        }
+        else
+        {
             int curr = st.top();
             st.pop();
-            if(mini<curr){
+            if (mini < curr)
+            {
                 return curr;
-            }else{
-                int prevMin = 2*st.top() - curr;
-                mini=prevMin;
+            }
+            else
+            {
+                int prevMin = mini;
+                int val = 2 * mini - curr;
+                mini = val;
                 return prevMin;
             }
         }
     }
-    int top(){
-
+    int top()
+    {
+        if (st.empty())
+        {
+            return -1;
+        }
+        else
+        {
+            int curr = st.top();
+            if (curr > mini)
+            {
+                return curr;
+            }
+            else
+            {
+                return mini;
+            }
+        }
     }
-    bool isEmpty(){
-
+    bool isEmpty()
+    {
+        return st.empty();
+    }
+    int getMin()
+    {
+        if (st.empty())
+        {
+            return -1;
+        }
+        else
+        {
+            return mini;
+        }
     }
 };
 
-int main(){
+int main()
+{
     stack<int> s;
 
     return 0;
